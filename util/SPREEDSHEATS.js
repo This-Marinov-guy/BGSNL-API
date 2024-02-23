ea2843733182e23cb4b9ffaf0d8f092b29536bea
This-Marinov-guy <vladislavmarinov3142@gmail.com>
2024-02-23 07:20:14 +0200

    modified:   util/SPREEDSHEATS.js

--- util/SPREEDSHEATS.js
+++ util/SPREEDSHEATS.js
@@ -1,14 +1,18 @@
 export const SPREADSHEETS_ID = {
     groningen: {
-        events : '1tSV3lF9NTWQrBam_SEt50rM91vdYR68Mx2OCe96OKKI',
+        events: '1tSV3lF9NTWQrBam_SEt50rM91vdYR68Mx2OCe96OKKI',
         users: '1tSV3lF9NTWQrBam_SEt50rM91vdYR68Mx2OCe96OKKI'
     },
     rotterdam: {
-        events : '1PEWOKkkrjDAuW30p2pgThQQZFQWMwO7n2gIa1KIF-i8',
+        events: '1PEWOKkkrjDAuW30p2pgThQQZFQWMwO7n2gIa1KIF-i8',
         users: '1LXqEhn6--T_dl2jTvMdfwKlXGj-svoC_wdVKAupVa5Y'
     },
     leeuwarden: {
-        events : '1FHWWtHW1U2na8inuf_xn2w7oK7rsSW568KmebnXTwHY',
+        events: '1FHWWtHW1U2na8inuf_xn2w7oK7rsSW568KmebnXTwHY',
+        users: ''
+    },
+    breda: {
+        events: '1JPUNqtCsfINUqAA9jGBUUd-a9DnxmKpqaw9X3z5qKbs',
         users: ''
     }
 }
