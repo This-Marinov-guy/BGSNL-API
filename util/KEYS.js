afce2fb20919f34d615694f30c9290ec5d9dd7df
This-Marinov-guy <vladislavmarinov3142@gmail.com>
2024-03-04 22:35:36 +0200

    modified:   util/KEYS.js

--- util/KEYS.js
+++ util/KEYS.js
@@ -1,41 +1,8 @@
 export const MEMBER_KEYS = [
     {
-        email: 'viktoria.sugareva@gmail.com', key: 'bgsr-viki'
-    },
-    {
-        email: 'kdimitrova8322@gmail.com', key: 'bgsr-kali'
-    },
-    {
-        email: 'pavelangelov77@gmail.com', key: 'bgsr-pavel'
-    },
-    {
-        email: 'alex.garcheva@gmail.com', key: 'bgsr-alex'
-    },
-    {
-        email: 'borisjpetrov03@gmail.com', key: 'bgsr-boris'
-    },
-    {
-        email: 'denisdimitrov693@gmail.com', key: 'bgsr-denis'
-    },
-    {
-        email: 'kiril.grozdev03@gmail.com', key: 'bgsr-kiril'
-    },
-    {
-        email: 'paolinagocheva@gmail.com', key: 'bgsr-paolina'
-    },
-    {
-        email: 'geri.stoyancheva@gmail.com', key: 'bgsr-geri'
-    },
-    {
-        email: 'varbanovalora@gmail.com', key: 'bgsr-lora'
-    },
-    {
-        email: 'vikisugareva@gmail.com', key: 'bgsr-viki'
-    },
-    {
-        email: 'geri.stoyancheva@gmail.com', key: 'bgsr-geri'
-    },
-    {
         email: 'bozhidar.bonev@yahoo.com', key: 'bgsr-boji'
+    },
+    {
+        email: 'nickstoychevbusiness@gmail.com', key: 'bgsg-nick'
     }
 ];
