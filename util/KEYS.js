929c50534fe82d787bd4a0c20c1171075143801e
This-Marinov-guy <vladislavmarinov3142@gmail.com>
2024-03-17 18:45:41 +0200

    modified:   util/KEYS.js

--- util/KEYS.js
+++ util/KEYS.js
@@ -5,4 +5,34 @@ export const MEMBER_KEYS = [
     {
         email: 'nickstoychevbusiness@gmail.com', key: 'bgsg-nick'
     },
+    {
+        email: 'martinnapetkova16@gmail.com', key: 'bgsa-marti'
+    },
+    {
+        email: 'annabayraktarska@gmail.com', key: 'bgsa-anna'
+    },
+    {
+        email: 'adrianakancheva@gmail.com', key: 'bgsa-adriana'
+    },
+    {
+        email: 'iveto0638@gmail.com', key: 'bgsa-iveto'
+    },
+    {
+        email: 'n.nedeva05@gmail.com', key: 'bgsa-nedeva'
+    },
+    {
+        email: 'daviddimkata7@gmail.com', key: 'bgsa-david'
+    },
+    {
+        email: 'ivan.zdravkov97@gmail.com', key: 'bgsa-ivan'
+    },
+    {
+        email: 'lusiii.krusteva@gmail.com', key: 'bgsa-lusi'
+    },
+    {
+        email: 'ivadimova1123@gmail.com', key: 'bgsa-iva'
+    },
+    {
+        email: 'anton_tchavdarov@icloud.com', key: 'bgsa-anton'
+    },
 ];
