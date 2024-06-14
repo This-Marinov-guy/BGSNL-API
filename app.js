3048c26209971db9ffd0a4ebcc308d2ad3147926
Vladislav Marinov <104770295+This-Marinov-guy@users.noreply.github.com>
2024-06-14 11:02:26 +0300

    Update app.js

--- app.js
+++ app.js
@@ -28,8 +28,8 @@ const allowedOrigins = [
 ];
 
 const stripeUrls = [
- "https://dashboard.stripe.com",
- "https://dashboard.stripe.com/"
+"https://checkout.stripe.com/",
+"https://dashboard.stripe.com/"
 ];
 
 app.use((req, res, next) => {
