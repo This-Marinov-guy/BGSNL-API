a4104a1b454db9535c0274e5095aa694b48ef1a5
This-Marinov-guy <vladislavmarinov3142@gmail.com>
2024-09-16 20:10:51 +0300

    up

--- routes/common-routes.js
+++ routes/common-routes.js
@@ -5,14 +5,4 @@ dotenv.config();
 
 const commonRouter = express.Router();
 
-commonRouter.post(
-    "/register",
-    [check("name").notEmpty(),
-    check("surname").notEmpty(),
-    check("email").notEmpty()
-    ],
-    postAddParticipant
-);
-
-
 export default commonRouter;
