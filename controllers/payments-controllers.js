9d87b22b501df9c6fe36e088f6c88fe56ccdd665
This-Marinov-guy <vladislavmarinov3142@gmail.com>
2024-10-01 22:23:33 +0300

    test

--- controllers/payments-controllers.js
+++ controllers/payments-controllers.js
@@ -241,9 +241,8 @@ export const postCustomerPortal = async (req, res, next) => {
 };
 
 export const postWebhookCheckout = async (req, res, next) => {
-  const userRegion = req.query.region ?? "netherlands";
+  const userRegion = req.query.region ?? "test";
   console.log(userRegion);
-  //TODO: fix region
   const sig = req.headers["stripe-signature"];
   const endpointSecret = getStripeKey("webhookSecretKey", userRegion);
   const stripeClient = createStripeClient(userRegion);
