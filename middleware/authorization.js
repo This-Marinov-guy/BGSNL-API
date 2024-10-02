3d24ef9c9e44ce53555e2f83cd9cdf055d581a4a
This-Marinov-guy <vladislavmarinov3142@gmail.com>
2024-10-02 22:41:10 +0300

    up

--- middleware/authorization.js
+++ middleware/authorization.js
@@ -4,46 +4,46 @@ import jwt from "jsonwebtoken";
 import HttpError from "../models/Http-error.js";
 
 export const authMiddleware = (req, res, next) => {
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1]; // Bearer TOKEN
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1]; // Bearer TOKEN
 
-    if (!token) {
-        console.log(`No token!`);
-        return next(new HttpError('No access for such request', 401));
+  if (!token) {
+    console.log(`No token!`);
+    return next(new HttpError("Please login to access this request", 401));
+  }
+
+  jwt.verify(token, process.env.JWT_STRING, (err, user) => {
+    if (err) {
+      console.log(`Token: ${token} | Error: ${err}`);
+      return next(new HttpError("No access for such request", 403));
     }
 
-    jwt.verify(token, process.env.JWT_STRING, (err, user) => {
-        if (err) {
-            console.log(`Token: ${token} | Error: ${err}`);
-            return next(new HttpError('No access for such request', 403))
-        }
-
-        next(); 
-    });
+    next();
+  });
 };
 
 export const adminMiddleware = (requiredRoles = []) => {
-    return (req, res, next) => {
-        const authHeader = req.headers['authorization'];
-        const token = authHeader && authHeader.split(' ')[1]; // Bearer TOKEN
+  return (req, res, next) => {
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1]; // Bearer TOKEN
 
-        if (!token) {
-            console.log(`No token!`);
-            return next(new HttpError('No access for such request', 401));
-        }
+    if (!token) {
+      console.log(`No token!`);
+      return next(new HttpError("Please login to access this request", 401));
+    }
 
-        jwt.verify(token, process.env.JWT_STRING, (err, user) => {
-            if (err) {
-                console.log(`Token: ${token} | Error: ${err}`);
-                return next(new HttpError('No access for such request', 403));
-            }
+    jwt.verify(token, process.env.JWT_STRING, (err, user) => {
+      if (err) {
+        console.log(`Token: ${token} | Error: ${err}`);
+        return next(new HttpError("No access for such request", 403));
+      }
 
-            if (!requiredRoles.some(role => user['roles'].includes(role))) {
-                console.log(`Token: ${token} | Error: No role rights for such request`);
-                return next(new HttpError('No access for such request', 403));
-            }
+      if (!requiredRoles.some((role) => user["roles"].includes(role))) {
+        console.log(`Token: ${token} | Error: No role rights for such request`);
+        return next(new HttpError("No access for such request", 403));
+      }
 
-            next();
-        });
-    };
+      next();
+    });
+  };
 };
