2fcf125a3deeb2515281891aa528ba9875e5acae
This-Marinov-guy <vladislavmarinov3142@gmail.com>
2024-10-05 13:18:21 +0300

    уп

--- util/private/manipulate-db.js
+++ util/private/manipulate-db.js
@@ -1,6 +1,12 @@
-import moment from 'moment';
-import { MongoClient } from 'mongodb';
-import { ADMIN, BOARD_MEMBER, COMMITTEE_MEMBER, MEMBER, VIP } from '../config/defines.js';
+import moment from "moment";
+import { MongoClient } from "mongodb";
+import {
+  ADMIN,
+  BOARD_MEMBER,
+  COMMITTEE_MEMBER,
+  MEMBER,
+  VIP,
+} from "../config/defines.js";
 
 // Connection URI
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB}`;
@@ -9,49 +15,53 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${proce
 const client = new MongoClient(uri);
 
 export async function updateUsers() {
-    try {
-        // Connect to the MongoDB server
-        await client.connect();
-        console.log("Connected successfully to server");
+  try {
+    // Connect to the MongoDB server
+    await client.connect();
+    console.log("Connected successfully to server");
 
-        // Get the database and collection
-        const database = client.db();
-        const users = database.collection("users");
-        const events = database.collection("events");
+    // Get the database and collection
+    const database = client.db();
+    const users = database.collection("users");
+    const events = database.collection("events");
 
-        const emailsToFind = []
+    const emailsToFind = [
 
-        // Find all users
-        // const cursor = users.find({ email: { $in: emailsToFind } });
-        // const cursor = users.find();
+    ];
 
-        // // Iterate over all users
-        for await (const user of cursor) {
-            // Create a new object without the 'roles' property
-            const newUserDoc = { ...user };
+    // Find all users
+    const cursor = users.find({ email: { $in: emailsToFind } });
+    // const cursor = users.find();
 
-            // Find the position of 'status' and insert 'roles' after it
-            const keys = Object.keys(newUserDoc);
-            const statusIndex = keys.indexOf('status');
+    // // Iterate over all users
+    for await (const user of cursor) {
+      // Create a new object without the 'roles' property
+      const newUserDoc = { ...user };
 
-            // Convert fields to Date objects
+      // Find the position of 'status' and insert 'roles' after it
+      const keys = Object.keys(newUserDoc);
+      const statusIndex = keys.indexOf("status");
 
-            const newObj = {};
-            keys.forEach((key, index) => {
-                newObj[key] = newUserDoc[key];
-            });
-            
-            newObj.roles = [BOARD_MEMBER]
+      // Convert fields to Date objects
 
-            delete newUserDoc._id;  // _id is immutable, so we remove it
+      const newObj = {};
+      keys.forEach((key, index) => {
+        newObj[key] = newUserDoc[key];
+      });
 
-            // Update the user
-            const result = await users.replaceOne({ _id: user._id }, newObj);
-            console.log(`Updated user ${user.name} ${user.surname} | ${user._id}: ${result.modifiedCount} document(s) modified`);
-        }
-    } catch (err) {
-        console.log(err);
-    } finally {
-        console.log('Script executed successfully');
+      newObj.roles = [BOARD_MEMBER];
+
+      delete newUserDoc._id; // _id is immutable, so we remove it
+
+      // Update the user
+      const result = await users.replaceOne({ _id: user._id }, newObj);
+      console.log(
+        `Updated user ${user.name} ${user.surname} | ${user._id}: ${result.modifiedCount} document(s) modified`
+      );
     }
+  } catch (err) {
+    console.log(err);
+  } finally {
+    console.log("Script executed successfully");
+  }
 }
