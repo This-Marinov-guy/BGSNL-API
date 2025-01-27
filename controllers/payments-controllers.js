c9c86a6530f850e70b8f32b15b685b33246c285f
This-Marinov-guy <vladislavmarinov3142@gmail.com>
2025-01-27 10:01:21 +0200

    revert last revision

--- controllers/payments-controllers.js
+++ controllers/payments-controllers.js
@@ -151,7 +151,6 @@ export const postCheckoutNoFile = async (req, res, next) => {
       lineItems.push({
         price: addOns[i].priceId,
         quantity: addOns[i].quantity ?? 1,
-        description: addOns[i].title ?? "Add-on",
       });
     }
   }
@@ -192,7 +191,6 @@ export const postCheckoutFile = async (req, res, next) => {
       lineItems.push({
         price: addOns[i].priceId,
         quantity: addOns[i].quantity ?? 1,
-        description: addOns[i].title ?? "Add-on",
       });
     }
   }
